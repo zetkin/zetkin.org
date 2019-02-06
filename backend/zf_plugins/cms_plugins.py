dffe605c4ba830c1c155989d088d6c10f5c4d92a
Richard Olsson <r@richardolsson.se>
2019-02-06 11:37:00 +0100

    Add CMS plugin placeholders for design

--- backend/zf_plugins/cms_plugins.py
+++ backend/zf_plugins/cms_plugins.py
@@ -2,14 +2,72 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
 @plugin_pool.register_plugin
-class Blurb(CMSPluginBase):
+class ArticlePlugin(CMSPluginBase):
+    allow_children = True
+    child_classes = ['TextPlugin', 'PicturePlugin',]
+    name = 'Article'
+    render_template = 'zf_plugins/article.html'
+
+
+@plugin_pool.register_plugin
+class BlurbPlugin(CMSPluginBase):
     name = 'Blurb'
     render_template = 'zf_plugins/blurb.html'
 
 
 @plugin_pool.register_plugin
-class BlurbGrid(CMSPluginBase):
+class BlurbGridPlugin(CMSPluginBase):
     allow_children = True
-    child_classes = ['Blurb']
+    child_classes = ['BlurbPlugin']
     name = 'Blurb Grid'
-    render_template = 'zf_plugins/grid.html'
+    render_template = 'zf_plugins/blurb_grid.html'
+
+
+@plugin_pool.register_plugin
+class FAQAccordionPlugin(CMSPluginBase):
+    name = 'FAQ Accordion'
+    render_template = 'zf_plugins/faq_accordion.html'
+
+
+@plugin_pool.register_plugin
+class FeaturePlugin(CMSPluginBase):
+    name = 'Feature'
+    render_template = 'zf_plugins/feature.html'
+
+
+@plugin_pool.register_plugin
+class FeatureGridPlugin(CMSPluginBase):
+    name = 'Feature Grid'
+    allow_children = True
+    child_classes = ['FeaturePlugin']
+    render_template = 'zf_plugins/feature_grid.html'
+
+
+@plugin_pool.register_plugin
+class HistoryTimelinePlugin(CMSPluginBase):
+    name = 'History Timeline'
+    render_template = 'zf_plugins/history_timeline.html'
+
+
+@plugin_pool.register_plugin
+class MediaHeroPlugin(CMSPluginBase):
+    name = 'Media Hero'
+    render_template = 'zf_plugins/media_hero.html'
+
+
+@plugin_pool.register_plugin
+class QuoteHeroPlugin(CMSPluginBase):
+    name = 'Quote Hero'
+    render_template = 'zf_plugins/quote_hero.html'
+
+
+@plugin_pool.register_plugin
+class SectionHeadPlugin(CMSPluginBase):
+    name = 'Section Head'
+    render_template = 'zf_plugins/section_head.html'
+
+
+@plugin_pool.register_plugin
+class SimpleHeroPlugin(CMSPluginBase):
+    name = 'Simple Hero'
+    render_template = 'zf_plugins/simple_hero.html'
